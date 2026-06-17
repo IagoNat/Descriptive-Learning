119dcc5659ebeb80be1beec110fa5b4326cc9789
IagoNat <iago.nathan.dev@gmail.com>
2026-06-17 17:10:49 -0300

    changes

--- dictionaries/registry.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-import pandas as pd
-
-class DictionaryRegistry:
-    def __init__(self, base_path="gov/rais/dictionaries"):
-        self.base_path = Path(base_path)
-
-    def load(self, column_name: str) -> pd.DataFrame:
-        return pd.read_csv(self.base_path/f"{column_name}.csv")
